4c5f7b1f2844200e95a1a6ce151a57770a6064ba
ivankleyton <ivankleyton12@gmail.com>
2026-07-23 14:19:10 -0300

    Melhora visual do cabecalho

--- app/components/Header.js
+++ app/components/Header.js
@@ -3,16 +3,27 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <a className={styles.logo} href="#inicio">
-        KCF
-      </a>
+      <div className={styles.container}>
+        <a className={styles.logo} href="#inicio">
+          KCF<span>.</span>
+        </a>
 
-      <nav className={styles.navigation}>
-        <a href="#inicio">Início</a>
-        <a href="#sobre">Sobre</a>
-        <a href="#projetos">Projetos</a>
-        <a href="#contato">Contato</a>
-      </nav>
+        <nav className={styles.navigation}>
+          <a className={styles.active} href="#inicio">
+            Início
+          </a>
+
+          <a href="#sobre">Sobre mim</a>
+          <a href="#tecnologias">Tecnologias</a>
+          <a href="#experiencias">Experiências</a>
+          <a href="#projetos">Projetos</a>
+          <a href="#contato">Contato</a>
+        </nav>
+
+        <a className={styles.contactButton} href="#contato">
+          Vamos conversar
+        </a>
+      </div>
     </header>
   );
 }
